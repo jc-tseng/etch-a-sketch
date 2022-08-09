98c4c13d7387b2392d3506f484def3b5c5c89f27
Jonathan Tseng <jonathanchristiantseng@gmail.com>
2022-08-08 17:16:36 -0700

    added event listener to change divs if mouse hovers over

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,20 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.grid');
 
 function createGrid(x, y) {
     container.style.setProperty('--grid-rows', x);
     container.style.setProperty('--grid-cols', y);
     for (let i = 0; i < (x * y); i++){
         const cell = document.createElement('div');
-        cell.innerText = i;
         container.appendChild(cell).className = 'grid-item';
     }
 }
 
 createGrid(16,16);
+
+const items = document.getElementsByClassName('grid-item');
+
+for (const item of items) {
+    item.addEventListener('mouseover', () => {
+        item.style.backgroundColor = 'black';
+    })
+}
