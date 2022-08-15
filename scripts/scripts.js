7ff0aad01c195684a2429e6c8a85405ebeb73811
Jonathan Tseng <jonathanchristiantseng@gmail.com>
2022-08-15 15:26:19 -0700

    fixed grid to stay 500 x 500px

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,20 +1,29 @@
-const container = document.querySelector('.grid');
-
-function createGrid(x, y) {
+function createGrid(x) {
+    const container = document.querySelector('.grid');
+    const cells = container.querySelectorAll('div');
+    cells.forEach((div) => div.remove());
     container.style.setProperty('--grid-rows', x);
-    container.style.setProperty('--grid-cols', y);
-    for (let i = 0; i < (x * y); i++){
+    container.style.setProperty('--grid-cols', x);
+
+    for (let i = 0; i < (x * x); i++){
         const cell = document.createElement('div');
         container.appendChild(cell).className = 'grid-item';
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = 'black';
+        })
     }
 }
 
-createGrid(16,16);
+createGrid(16);
 
 const items = document.getElementsByClassName('grid-item');
 
-for (const item of items) {
-    item.addEventListener('mouseover', () => {
-        item.style.backgroundColor = 'black';
-    })
-}
+const set_grid = document.querySelector('.set-grid');
+
+set_grid.addEventListener('click', () => {
+    input = prompt('Enter number of squares per side of grid (max 100):');
+    while (input !== null && (isNaN(input) || !(input >= 1 && input <= 100))) {
+        input = prompt('Enter number of squares per side of grid (max 100):');
+    }
+    createGrid(input);
+})
